d183bf0425199153ab393fbf9458aaff83c60e41
GeorgeDzigo <mr.georgegamertv@gmail.com>
2020-09-07 16:30:21 +0300

    All sections have resolution till 500px, except footer (I am still working on it)

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ session_start();
       <title></title>
       <!-- Main -->
       <link rel="stylesheet" href="css/regist.css" type="text/css">
-      <!-- <link rel="stylesheet" href="css/style.css" type="text/css"> -->
-      <link rel="stylesheet" href="css/style2.css" type="text/css">
+      <link rel="stylesheet" href="css/style.css" type="text/css">
+      <!-- <link rel="stylesheet" href="css/style2.css" type="text/css"> -->
       <link rel="stylesheet" href="css/counter.css">
       <link rel="stylesheet" href="responsive/css/responsive.css">
       <link rel="stylesheet" href="css/reset.css">
@@ -119,7 +119,11 @@ session_start();
                         <!-- Typed About me -->
 
                         <div id="about-me">
-
+                              <!-- Responsive me png-->
+                              <div class="about-me-res-div">
+                               <img src="img/me.png" alt="" id="res-about-me-img"> 
+                              </div>
+                              <!-- End Responsive me png -->
                               <span id="typed"></span>
                               <img src="img/me.png" id="aboutmeimg">
                         </div>
@@ -136,14 +140,51 @@ session_start();
       </div>
       <!-- Introduction -->
       <section id="intro">
-            <img src="img/hacky.png" alt="me" id="intro-img" class="">
+            <!-- Respsonsive -->
+
+            <center class="res-intro-img">
+                  <img src="img/hacky.png" alt="me" id="intro-img" class="">
+            </center>
+            <!-- End Responsive -->
+
+            <img src="img/hacky.png" alt="me" id="intro-img" class="none-res-intro-img">
+            
             <h3 class="intro-h3">MY INTRO</h3>
             <h1 class="intro-h1">ABOUT ME</h1>
             <pre class="intro-pre">
-                My Name is Giorgi Dzigoshvili I am a Web Developer in age 14 
+                My Name is Giorgi Dzigoshvili I am a Web Developer, 
                 almost FullStack Developer and This is one of my portfolio. 
             </pre>
+            <!-- Repsonsive -->
+            <pre class="res-intro-pre" id="res-intro-pre">
+                My name is Giorgi Dzigoshvili I am a
+                Web Developer, almost FullStack Developer
+                and This is one of my Web-Site/Portfolio.
+            </pre>
+            
+            <center class="center-res-intro-pre">
 
+            <pre class="res-intro-pre">
+            My name is Giorgi Dzigoshvili
+            I am a Web Developer, almost 
+       FullStack Developer and 
+                   This is one of my Web-Site/Portfolio.
+            </pre>
+            </center>
+
+            <!-- End Respsonsive -->
+
+            <pre class="intro-list" id="none-res-intro-list">
+            <b class="intro-list-h3">Name:  <i class="intro-list-i">George Dzigo</i></b><br>
+            <b class="intro-list-h3">Date Of Birth:  <i class="intro-list-i"> 10.06.2006</i></b><br> 
+            <b class="intro-list-h3">Address:  <i class="intro-list-i"> Athens,Greece</i></b><br>
+            <b class="intro-list-h3">ZipCode:  <i class="intro-list-i"> 1000</i></b><br>  
+            <b class="intro-list-h3">E-mail:  <i class="intro-list-i"> mr.georgegamertv@gmail.com</i></b><br>
+            <b class="intro-list-h3">Phone:  <i class="intro-list-i"> +30-6996-8917-99</i></b><br>
+            </pre>
+
+            <!-- Responsive -->
+            <center class="center-intro-list">
             <pre class="intro-list">
             <b class="intro-list-h3">Name:  <i class="intro-list-i">George Dzigo</i></b><br>
             <b class="intro-list-h3">Date Of Birth:  <i class="intro-list-i"> 10.06.2006</i></b><br> 
@@ -152,7 +193,13 @@ session_start();
             <b class="intro-list-h3">E-mail:  <i class="intro-list-i"> mr.georgegamertv@gmail.com</i></b><br>
             <b class="intro-list-h3">Phone:  <i class="intro-list-i"> +30-6996-8917-99</i></b><br>
             </pre>
-            <img src="img/sc.png" alt="" class="intro-hobbies">
+            </center>
+
+            <center class="center-intro-hobbies">
+                  <img src="img/sc.png" alt="" class="intro-hobbies">
+            </center>
+            <!-- End Respsonive -->
+            <img src="img/sc.png" alt="" class="intro-hobbies" id="none-res-intro-hobbies">
       </section>
 
 
@@ -425,7 +472,7 @@ session_start();
             <!-- End Services Titles -->
 
             <!-- Web Design -->
-
+            <center>
             <div id="services-container">
                   <div id="services-container-box">
                         <div id="services-container-logo-holder">
@@ -543,13 +590,14 @@ session_start();
                         </p>
                   </div>
             </div>
-
+            </center>
       </section>
 
 
       <!-- End Services -->
 
       <!-- Counters -->
+      <center>
       <section class="counters">
             <div class="container">
                   <div class="counter-holder">
@@ -570,12 +618,13 @@ session_start();
 
                         <h3 class="counter-h3" id="counter-holder-counter-h3-2">Happy Customers</h3>
                   </div>
+                  <div class="counters-block"></div>
                   <div class="counter-holder">
                         <div class="counters-logo-holder">
                               <img src="https://img.icons8.com/carbon-copy/70/000000/espresso-cup.png" />
                         </div>
 
-                        <div class="counter" id="counter-holder-counter-3" data-target="143">0</div>
+                        <div class="counter" id="counter-holder-counter-3" data-target="1213">0</div>
 
                         <h3 class="counter-h3" id="counter-holder-counter-h3-3">Cups Of Coffee</h3>
                   </div>
@@ -590,6 +639,7 @@ session_start();
                   </div>
             </div>
       </section>
+      </center>
       <!-- End Counters -->
 
       <!-- Projects -->
@@ -605,6 +655,7 @@ session_start();
                         and Consonantia</p>
             </div>
             <!-- End Projects Titles -->
+            <center>
             <div id="projects-container">
                   <!-- Projects box 1 -->
                   <div class="projects-container-box">
@@ -656,7 +707,7 @@ session_start();
 
                   <!-- Projects box 4 -->
 
-                  <div class="projects-container-box" style="margin-left: 350px">
+                  <div class="projects-container-box">
                         <div class="projects-container-box-logo-holder" id="projects-holder4">
                               <img src="projectsimg/4.jpg" alt="" id="projects-img4">
                         </div>
@@ -703,7 +754,7 @@ session_start();
                   <!-- End Projects box 6 -->
 
             </div>
-
+            </center>
       </section>
 
       <!-- End Projects -->
@@ -712,19 +763,30 @@ session_start();
 
       <!-- Project Offer Section -->
       <br><br><br><br><br><br><br><br>
+      <center>
       <section id="project-offer-section">
-            <div class="project-offer-section-text">
-                  <h1 class="project-offer-section-text-h1" id="typed2">Have a project on your mind</h1>
+            <div class="project-offer-section-text" id="none-res-project-offer-section-text">
+                  <h1 class="project-offer-section-text-h1" id="typed2"></h1>
 
-                  <p class="project-offer-section-text-p" id="typed-p">A small river named Duden flows by their place
-                        and supplies it
-                        with the necessary<br>
-                        regelialia. It is a paradisematic country, in which roasted parts of sentences fly.</p>
+                  <p class="project-offer-section-text-p" id="typed-p"></p>
                   <div class="projects-offer-section-button">Contact Me</div>
             </div>
-            <img src="img/me.png" alt="" class="project-offer-section-img">
+
+            <img src="img/me.png" alt="" class="project-offer-section-img" id="none-res-offer-section-img">
+  
+            <!-- Responsive  -->
+            <center id="res-offer-section-img">
+                  <img src="img/me.png" alt="" class="project-offer-section-img" >
+            </center>
+            <center  id="res-project-offer-section-text">
+                  <h1 class="project-offer-section-text-h1" id="typed2">Have a project on your mind</h1>
+                  <p class="project-offer-section-text-p" id="typed-p">If Yes, What are you waiting for then</p>
+                  <div class="projects-offer-section-button">Contact Me</div>
+            </center>
+            <!-- End Responsive -->
 
       </section>
+      </center>
       <!-- End Project Offer Section -->
 
 
@@ -738,7 +800,7 @@ session_start();
                         and <br> Consonantia</p>
             </div>
 
-            <article class="contacts-article">
+            <article class="contacts-article" id="none-contacts-article">
                   <form action="mail/mail.php" method="post" class="contacts-article-form">
                         <input type="text" name="name" min="5" placeholder="Name" id="contact-form-name" required>
                         <input type="email" name="email" placeholder="Email" id="contact-form-email" required>
@@ -747,6 +809,21 @@ session_start();
                         <input type="submit" id="contact-form-submit" value="Send Message">
                   </form>
             </article>
+
+            <!-- Responsive -->
+            <center class="res-contacts-article">
+                  <article class="contacts-article">
+                        <form action="mail/mail.php" method="post" class="contacts-article-form">
+                              <input type="text" name="name" min="5" placeholder="Name" id="contact-form-name" required>
+                              <input type="email" name="email" placeholder="Email" id="contact-form-email" required>
+                              <input type="text" name="subject" placeholder="Subject" id="contact-form-subject">
+                              <textarea name="message" cols="30" rows="10" placeholder="message" id="contact-form-message"></textarea>
+                              <input type="submit" id="contact-form-submit" value="Send Message">
+                        </form>
+                  </article>
+            </center>
+
+            <!-- End Responsive -->
 
             <!-- Contacts Aside -->
             <aside class="contacts-aside">
@@ -782,8 +859,7 @@ session_start();
 
       <!-- Footer -->
       <footer id="footer">
-
-            <div class="footer-wrapper">
+            <div class="footer-wrapper" id="none-footer-wrapper">
                   <div class="footer-aboutme footer-box">
                         <h2 class="footer-aboutme-h2">Lets Talk about</h2>
                         <pre class="footer-aboutme-pre">
@@ -847,6 +923,77 @@ session_start();
 
                   <!-- End Social Media IMGS -->
             </div>
+
+            <!-- Responsive -->
+                 <center class="res-footer-wrapper">
+                 <center>
+                  <div class="footer-wrapper">
+                        <div class="footer-aboutme footer-box">
+                              <h2 class="footer-aboutme-h2">Lets Talk about</h2>
+                              <pre class="footer-aboutme-pre">
+                              Far far away, behind the word
+                              mountains, far from the
+                              countries Vokalia and
+                              Consonantia, there live the
+                              blind texts.
+                        </pre>
+                              <a href="#contacts" class="footer-aboutme-a">Contact Me</a>
+                  </div>
+                  </center>
+
+                  <center>
+                  <div class="footer-links footer-box">
+                        <h2 class="footer-links-h2">Links</h2>
+                        <pre class="footer-links-pre">
+                 <b>></b> <a href="#logo-holder" class="footer-links-pre-a">Home</a>
+                 <b>></b> <a href="#intro" class="footer-links-pre-a">About</a>
+                 <b>></b> <a href="#myskills" class="footer-links-pre-a">Skills</a>
+                 <b>></b> <a href="#services" class="footer-links-pre-a">Services</a>
+                 <b>></b> <a href="#projects" class="footer-links-pre-a">Projects</a>
+                 <b>></b> <a href="#contacts" class="footer-links-pre-a">Contacts</a>
+            </pre>
+                  </div>
+                  </center>
+                  <center>
+                  <div class="footer-services footer-box">
+                        <h2 class="services-links-h2">Services</h2>
+                        <pre class="services-links-pre">
+                              <b>></b> <a href="#services" class="services-links-pre-a">Web Design</a>
+                              <b>></b> <a href="#services" class="services-links-pre-a">Web Development</a>
+                              <b>></b> <a href="#services" class="services-links-pre-a">Business</a>
+                              <b>></b> <a href="#services" class="services-links-pre-a">Data Analysis</a>
+                              <b>></b> <a href="#services" class="services-links-pre-a">Graphic Design</a>
+                              <b>></b> <a href="#services" class="services-links-pre-a">Seo</a>
+                         </pre>
+
+                  </div>
+                  </center>
+                  <center>
+                  <div class="footer-questions footer-box">
+                        <h2 class="footer-questions-h2">Have a Question?</h2>
+                        <pre class="footer-questions-pre">
+                  <img src="https://img.icons8.com/carbon-copy/30/000000/map-marker.png"/>203 Fake St. Mountain
+                      View, San Francisco,
+                      California, USA
+
+                  <img src="https://img.icons8.com/carbon-copy/30/000000/phone.png"/> +2 392 3929 215
+
+                  <img src="https://img.icons8.com/ios/20/000000/paper-plane.png"/>  info@yourdomain.com
+            </pre>
+                  </div>
+                  </center>
+                  <!-- Social Media IMGS -->
+                  <center>
+                        <div class="social-media-img-holder">
+                              <a href="https://www.facebook.com/kitchendesign.ge/?ref=aymt_homepage_panel&eid=ARBjw_LEKG3o8typ58M-ZHLE8Stc1qrexqFrUKQo__co14sYDthYo-Qp1h74MBzG6c4SRNRh2Wv2YdbK" class="wow fadeIn social-media-a" target="_blank"><img src="https://img.icons8.com/dusk/35/000000/facebook-new--v2.png" class="social-meadia-img-a-img" /></a>
+                              <img src="https://img.icons8.com/office/35/000000/instagram-new.png" class="social-meadia-img-a-img" />
+                              <img src="https://img.icons8.com/doodle/35/000000/github--v1.png" class="social-meadia-img-a-img" />
+                        </div>
+                  </center>
+                  <!-- End Social Media IMGS -->
+            </div>
+                 </center>
+            <!-- End Responsive -->
       </footer>
       <!-- End Footer -->
 
